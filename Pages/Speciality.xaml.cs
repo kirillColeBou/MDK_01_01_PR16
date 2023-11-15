59a10d06eb09464036f73cf2870c446b4dbe1277
student-a202 <student-a202@PC-A202-1>
2023-11-15 15:13:24 +0500

    Готовая страница Speciality. Тепляков

---
+++ Pages/Speciality.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Submission_of_Applications_Тепляков.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Speciality.xaml
+    /// </summary>
+    public partial class Speciality : Page
+    {
+        public Speciality()
+        {
+            InitializeComponent();
+        }
+    }
+}
